e138ec99670db092638eee4fe7feb171a1355a13
Mansoor Basha Syed <83864755+mansoorsd@users.noreply.github.com>
2021-09-03 08:53:38 +0530

    Update connect_to_database.java

--- connect_to_database.java
+++ connect_to_database.java
@@ -1,1 +1,19 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/mansoor/IdeaProjects/SQLite_p/TestDB.db");
+        }
+        catch (SQLException e) {
+            System.out.println("Something went wrong" +e.getMessage());
+
+        }
+    }
+}
 
